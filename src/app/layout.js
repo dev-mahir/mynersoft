074317f19806a8a2f33044a9175bc1d9599a1813
dev-mahir <mdmahir.ds@gmail.com>
2024-06-25 08:08:20 +0600

    add vercel file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
+		<>
+			<html lang="en">
+				<body className={inter.className}>
+					{children}
+					<SpeedInsights />
+				</body>
+			</html>
+		</>
 	);
 }
