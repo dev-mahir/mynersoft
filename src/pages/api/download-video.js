a2bea89af902fb02f17c98b636c96a58ca2635f0
dev-mahir <mdmahir.ds@gmail.com>
2024-07-13 22:55:24 +0600

    add downloader for yt videos

--- src/pages/api/download-video.js
+++ src/pages/api/download-video.js
@@ -1,39 +1,12 @@
-// pages/api/download-video.js
 import ytdl from "ytdl-core";
-import fs from "fs";
-import path from "path";
 
-export default async function handler(req, res) {
+export default async () => {
 	const { url } = req.query;
-
-	if (!url || !ytdl.validateURL(url)) {
-		res.status(400).json({ error: "A valid URL is required" });
-		return;
-	}
-
-	const videoPath = path.join(process.cwd(), "public", "video.mp4");
-
-	try {
-		const videoStream = ytdl(url, { filter: "audioandvideo" }).pipe(
-			fs.createWriteStream(videoPath)
-		);
-
-		videoStream.on("finish", () => {
-			res.status(200).json({
-				message: "Download complete",
-				videoPath: "/video.mp4",
-			});
-		});
-
-		videoStream.on("error", (err) => {
-			console.error("Error downloading video:", err);
-			res.status(500).json({
-				error: "Error downloading video",
-				details: err.message,
-			});
-		});
-	} catch (err) {
-		console.error("Error:", err);
-		res.status(500).json({ error: "Server error", details: err.message });
-	}
-}
+	ytdl(url).pipe(
+		fs.createWriteStream("video.mp4")
+	);
+	const video = ytdl(url, { filter: "audioonly" });
+	res.setHeader("Content-Disposition", 'attachment; filename="video.mp4"');
+	res.setHeader("Content-Type", "audio/mp4");
+	video.pipe(res);
+};
